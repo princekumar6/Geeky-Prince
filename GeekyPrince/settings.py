f26e35e81621f8dbb10b42a3e1f4a16bb338ef4c
princekumar6 <princerajputana5@gmail.com>
2020-06-03 14:55:55 +0530

    first commit

--- GeekyPrince/settings.py
+++ GeekyPrince/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 from django.contrib.messages import constants as messages
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
